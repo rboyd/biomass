37863956267aa3013d00807b32adebea6d956b90
Robert Boyd <robert.boyd@me.com>
2013-02-23 15:37:40 -0600

    Updating project.clj, bringing in clj-http.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject biomass "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Boss people around. No suit required."
+  :url "https://github.com/rboyd/biomass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.6.4"]])
